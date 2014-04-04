2e6a3df9f52ff7ee04cfcbc4f3bf6975e2111796
Joris Vandeweerd <joris@statik.be>
2014-04-04 14:51:11 +0200

    added processView to set js and css of the form to the view

--- src/ride/web/form/WebForm.php
+++ src/ride/web/form/WebForm.php
@@ -5,6 +5,8 @@ namespace ride\web\form;
 use ride\library\form\exception\FormException;
 use ride\library\form\AbstractComponentForm;
 use ride\library\http\Request;
+use ride\library\mvc\view\HtmlView;
+use ride\library\mvc\view\View;
 
 /**
  * Web implementation of a form
@@ -77,6 +79,41 @@ class WebForm extends AbstractComponentForm {
         $this->buildRows($data);
 
         return $this;
+    }
+
+    /**
+     * Processes the assets of the form into the HTML view
+     * @param \ride\library\mvc\view\View $view View on which the form is
+     * displayed
+     * @return null
+     */
+    public function processView(View $view) {
+        if (!$this->rows || !$view instanceof HtmlView) {
+            return;
+        }
+
+        foreach ($this->rows as $row) {
+            $javascripts = $row->getJavascripts();
+            if ($javascripts) {
+                foreach ($javascripts as $javascript) {
+                    $view->addJavascript($javascript);
+                }
+            }
+
+            $inlineJavascripts = $row->getInlineJavascripts();
+            if ($inlineJavascripts) {
+                foreach ($inlineJavascripts as $inlineJavascript) {
+                    $view->addInlineJavascript($inlineJavascript);
+                }
+            }
+
+            $styles = $row->getStyles();
+            if ($styles) {
+                foreach ($styles as $style) {
+                    $view->addStyle($style);
+                }
+            }
+        }
     }
 
 }
