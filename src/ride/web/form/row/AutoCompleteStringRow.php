eadff901e2743c6cccf02bd742128e546480ba80
Kaya <kayalion@kayalion.net>
2016-01-22 13:46:32 +0100

    added locale to auto complete row

--- src/ride/web/form/row/AutoCompleteStringRow.php
+++ src/ride/web/form/row/AutoCompleteStringRow.php
@@ -34,6 +34,12 @@ class AutoCompleteStringRow extends StringRow {
     const OPTION_AUTO_COMPLETE_TYPE = 'autocomplete.type';
 
     /**
+     * Name of the auto complete locale option
+     * @var string
+     */
+    const OPTION_LOCALE = 'locale';
+
+    /**
      * Creates the widget for this row
      * @param string $name
      * @param mixed $default
@@ -44,6 +50,7 @@ class AutoCompleteStringRow extends StringRow {
         $url = $this->getOption(self::OPTION_AUTO_COMPLETE_URL);
         if ($url) {
             $attributes['data-autocomplete-url'] = $url;
+            $attributes['data-autocomplete-locale'] = $this->getOption(self::OPTION_LOCALE);
             $attributes['data-autocomplete-multiple'] = $this->getOption(self::OPTION_AUTO_COMPLETE_MULTIPLE, false);
             $attributes['data-autocomplete-minimum'] = $this->getOption(self::OPTION_AUTO_COMPLETE_MINIMUM, 2);
             $attributes['data-autocomplete-type'] = $this->getOption(self::OPTION_AUTO_COMPLETE_TYPE, 'json');
